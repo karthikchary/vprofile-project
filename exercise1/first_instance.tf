e9b6abebc8e6fac119725f9f3163c1416d8a069f
karthik chary <karthik.chary138@gmail.com>
2021-11-04 18:19:33 +0530

    updated the ami and az

--- exercise1/first_instance.tf
+++ exercise1/first_instance.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-south-1"
   #   access_key = ""
   #   secret_key = ""	
 }
 
 resource "aws_instance" "intro" {
-  ami                    = "ami-03657b56516ab7912"
+  ami                    = "ami-0c1a7f89451184c8b"
   instance_type          = "t2.micro"
-  availability_zone      = "us-east-2a"
-  key_name               = "dove-key"
-  vpc_security_group_ids = ["sg-0780815f55104be8a"]
+  availability_zone      = "ap-south-1a"
+  key_name               = "kubernetes_mumbai"
+  vpc_security_group_ids = ["sg-0628ad93a7b58f5f5"]
   tags = {
     Name    = "Dove-Instance"
     Project = "Dove"
