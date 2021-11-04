4ad3231a88c80feed104d3c96e231291e12acd41
karthik chary <karthik.chary138@gmail.com>
2021-11-04 18:35:11 +0530

    updated the key name

--- exercise2/instance.tf
+++ exercise2/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "dove-inst" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
-  key_name               = "new-dove"
+  key_name               = "kubernetes_mumbai"
   vpc_security_group_ids = ["sg-0628ad93a7b58f5f5"]
   tags = {
     Name    = "Dove-Instance_terraform2"
