e4debde45e3a779cb198cdfdc346948a0b90028d
Karthik Chary Kondoju <kkondoju@evoketechnologies.com>
2025-08-02 14:13:09 +0530

    changed jenkins userdata

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
+sudo apt install openjdk-17-jdk -y
 sudo apt install ca-certificates -y
 sudo apt install maven git wget unzip -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+#sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
 ###
